0b448b95b63a524ca99c54d40fbf7432c68f956c
cralx2k <leao2004@gmail.com>
2025-09-30 22:44:51 -0500

    Log success message to verify_login_script.txt

--- login_test/show_login.ps1
+++ login_test/show_login.ps1
@@ -1,4 +1,5 @@
 $Username = $env:USERNAME
 Write-Host "Logged in as: $Username"
 Write-Host "Login is working! Welcome, cralx2k."
+"PowerShell script ran successfully" | Out-File -Append C:\temp\verify_login_script.txt
 Pause
